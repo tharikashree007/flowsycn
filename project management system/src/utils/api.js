ccbd7058cad364d4fdedbe8703b3e4f9fb4d165c
tharika <tharikashree007@gmail.com>
2026-01-31 22:48:08 +0530

    Update for separate frontend/backend deployment

--- project management system/src/utils/api.js
+++ project management system/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_URL = process.env.NODE_ENV === 'production' 
-  ? '/api' 
+  ? 'https://flowsync-backend.onrender.com/api' 
   : 'http://localhost:5000/api';
 
 const api = axios.create({
